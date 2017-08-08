b3b3d3710ab689d6f5261fd6ea5c922de05e8a9e
Erik Zakrzewski and Kristen Kulha <kristen@>
2017-08-08 13:44:01 -0700

    add structure to scripts.js file for appearing defenitions.

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $(".operators").click(function() {
+    $("#fadeToggle").fadeToggle();
+  });
+  $(".variables").click(function() {
+    $("#slideToggle").fadeToggle();
+  });
+  $(".functions").click(function() {
+    $("#fadeToggle").fadeToggle();
+  });
+  $(".methods").click(function() {
+    $("#slideToggle").fadeToggle();
+  });
+  $(".arguments").click(function() {
+    $("#fadeToggle").fadeToggle();
+  });
+  $(".parameters").click(function() {
+    $("#slideToggle").fadeToggle();
+  });
+  $(".strings").click(function() {
+    $("#fadeToggle").fadeToggle();
+  });
+  $(".booleans").click(function() {
+    $("#slideToggle").fadeToggle();
+  });
+  $(".attributes").click(function() {
+    $("#fadeToggle").fadeToggle();
+  });
+
+});
